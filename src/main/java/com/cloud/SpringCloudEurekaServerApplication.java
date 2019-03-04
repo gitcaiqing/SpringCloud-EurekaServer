cc8850ab8332d43be22758c92e15c94ecad82c8c
gitcaiqing <380120634@qq.com>
2019-03-04 17:24:32 +0800

    注册中心高可用

--- src/main/java/com/cloud/SpringCloudEurekaServerApplication.java
+++ src/main/java/com/cloud/SpringCloudEurekaServerApplication.java
@@ -1,7 +1,9 @@
 package com.cloud;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,7 +11,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class SpringCloudEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+		//SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+		// 读取控制台输入，决定使用哪个profiles
+		Scanner scan = new Scanner(System.in);
+		String profiles = scan.nextLine();
+		new SpringApplicationBuilder(SpringCloudEurekaServerApplication.class)
+			.properties("spring.profiles.active=" + profiles).web(true).run(args);
+		scan.close();
 	}
 
 }
